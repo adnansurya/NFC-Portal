22edc600687ad3a52e96397dd7413c97e4d70691
laodefit02 <lazy.mde4@gmail.com>
2019-12-21 18:01:40 +0800

    File Web (Source Code)
    File website cuk

---
+++ webFiles/script.js
@@ -0,0 +1,15 @@
+function pilihJenis() {
+     
+    var e = document.getElementById("JenisIdentitas");
+    var jenisId = e.options[e.selectedIndex].value;
+    if(jenisId == "home"){
+        document.getElementById("overlay").style.display = "none";
+    }else if(jenisId == "visitor"){
+        document.getElementById("overlay").style.display = "block";
+    }
+    
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
