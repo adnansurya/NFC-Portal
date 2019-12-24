0ea3f55a16bdb369e0bb42a193541882c5b4ace7
laodefit02 <lazy.mde4@gmail.com>
2019-12-24 17:22:01 +0800

    File Arduino
    File untuk arduino uno dan wemos, yang terupdate.

---
+++ arduino_files/NFCPark/NFCPark.ino
@@ -0,0 +1,78 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+
+// If using the breakout with SPI, define the pins for SPI communication.
+#define PN532_SCK  (2)
+#define PN532_MOSI (3)
+#define PN532_SS   (4)
+#define PN532_MISO (5)
+
+// If using the breakout or shield with I2C, define just the pins connected
+// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
+#define PN532_IRQ   (2)
+#define PN532_RESET (3)  // Not connected by default on the NFC Shield
+
+// Uncomment just _one_ line below depending on how your breakout or shield
+// is connected to the Arduino:
+
+// Use this line for a breakout with a software SPI connection (recommended):
+Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+// also change #define in Adafruit_PN532.cpp library file
+   #define Serial SerialUSB
+#endif
+
+void setup(void) {
+  #ifndef ESP8266
+    while (!Serial); // for Leonardo/Micro/Zero
+  #endif
+  Serial.begin(115200);
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Tidak Ditemukan");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+//  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+//  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+//  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+  
+//  Serial.println("Waiting for an ISO14443A Card ...");
+}
+
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  String idNFC = "";
+  if (success) {
+    // Display some basic information about the card
+//    Serial.println("Found an ISO14443A card");
+//    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+//    Serial.print("  UID Value: ");
+//    nfc.PrintHex(uid, uidLength);
+    for(int i=0; i<uidLength  ; i++){
+      idNFC += String(uid[i], HEX);
+      if(i < uidLength-1){
+        idNFC += ":";
+      }
+    }
+    Serial.println(idNFC);
+    
+  }
+}
